c3786401a91b294926a0aad398f01ee173b23e13
macoki07 <70043062+macoki07@users.noreply.github.com>
2022-10-11 17:29:30 +0800

    Update main.c

--- GamePrj/main.c
+++ GamePrj/main.c
@@ -10,10 +10,6 @@ void char_select_init(void);
 void char_select_update(void);
 void char_select_exit(void);
 
-void settings_init(void);
-void settings_update(void);
-void settings_exit(void);
-
 void game_init(void);
 void game_update(void);
 void game_exit(void);
