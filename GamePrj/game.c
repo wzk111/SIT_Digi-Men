8d32d83078715e2a943826c4e85dba05c3a7e44f
tzx2110 <tzx8787@gmail.com>
2022-10-11 20:28:45 +0800

    Update game.c
    Made the camera not exceed the game space. Added the CP_Image for all the character assets we use and loaded them. Free images at exit.

--- GamePrj/game.c
+++ GamePrj/game.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "cprocessing.h"
 
-#define GRID_SIZE 64
-#define GRID_ROW 48
-#define GRID_COL 48
+#define GRID_SIZE 128
+#define GRID_ROW 24
+#define GRID_COL 24
 double ROOM_HEIGHT = GRID_SIZE * GRID_ROW;
 double ROOM_WIDTH = GRID_SIZE * GRID_COL;
 
@@ -24,17 +24,6 @@ struct entity enemy2;
 and so on and so forth.
 */
 
-//making sure the player is in the center of the view
-//so we first need the create an imaginary camera
-struct camera {
-    double x;
-    double y;
-    int view_width; //view  width of the screen
-    int view_height; //view height of the entire screen
-    int camera_width; //camera width
-    int camera_height; //camera height
-};
-
 
 
 /* 
@@ -48,25 +37,66 @@ EVERYTHING that you use CP_DRAW for needs to have -camera1.x in its x coords. an
 
 If the camera system is too complicated i can remove it and just stick to a non moving screen.
 
-*/
+making sure the player is in the center of the view
+so we first need the create an imaginary camera */
 
-
-
-
+struct camera {
+    double x;
+    double y;
+    int view_width; //view  width of the screen
+    int view_height; //view height of the entire screen
+    int camera_width; //camera width
+    int camera_height; //camera height
+};
 
 struct camera camera1;
 
+
+//Creating a Grid to work with in the future. (2D array)
+GRID[GRID_COL][GRID_ROW];
+
+
+
+//Setting Images for Assets
+CP_Image PAWN;
+CP_Image KNIGHT;
+CP_Image BISHOP;
+CP_Image ROOK;
+CP_Image QUEEN;
+CP_Image KING;
+
+
+
+
+
 void game_init(void) {
 
-    //putting the player in the center of the room
-    Player.vector.x = ROOM_WIDTH / 2;
-    Player.vector.y = ROOM_HEIGHT / 2;
+    //loading in images
+    PAWN = CP_Image_Load("Assets/pawn.png");
+    KNIGHT = CP_Image_Load("Assets/knight.png");
+    BISHOP = CP_Image_Load("Assets/bishop.png");
+    ROOK = CP_Image_Load("Assets/rook.png");
+    QUEEN = CP_Image_Load("Assets/queen.png");
+    KING = CP_Image_Load("Assets/king.png");
 
-    camera1.view_height = GRID_SIZE * GRID_ROW / 2;
-    camera1.view_width = GRID_SIZE * GRID_COL / 3;
+
+    //putting the player in the center of the room
+    Player.vector.x = (float)ROOM_WIDTH / 2;
+    Player.vector.y = (float)ROOM_HEIGHT / 2;
     
+    //setting Player Stats
+    Player.DMG = 2;
+    Player.HP = 3;
+    Player.MOVE_SPEED = 10;
+
+    //setting camera view width and heights
+    camera1.view_height = GRID_SIZE * GRID_ROW / 3;
+    camera1.view_width = GRID_SIZE * GRID_COL / 2;
+    
+    //setting window size
     CP_System_SetWindowSize(GRID_SIZE * GRID_ROW/2, GRID_SIZE * GRID_COL/3);
 
+    //setting camera width and heights
     camera1.camera_width = camera1.view_width;
     camera1.camera_height = camera1.view_height;
 
@@ -76,10 +106,15 @@ void game_update(void) {
 
 
     //putting the camera at set distance from player and not exiting screen
-    camera1.x = Player.vector.x - camera1.camera_width/1.5;
-    camera1.y = Player.vector.y - camera1.camera_height/3;
+    camera1.x = Player.vector.x - camera1.camera_width/2;
+    camera1.y = Player.vector.y - camera1.camera_height/2;
 
 
+    //limiting the camera so it doesn't go out of bounds
+    if (camera1.x < 0) { camera1.x = 0; }
+    if ((camera1.x+camera1.camera_width) > ROOM_WIDTH) { camera1.x = ROOM_WIDTH-camera1.view_width; }
+    if (camera1.y < 0) { camera1.y = 0; }
+    if ((camera1.y+camera1.camera_height) > ROOM_HEIGHT) { camera1.y = ROOM_HEIGHT-camera1.camera_height; }
 
     //limiting the player so it doesn't go out of bounds
     if (Player.vector.x < 0) { Player.vector.x = 0; }
@@ -88,8 +123,10 @@ void game_update(void) {
     if (Player.vector.y > ROOM_HEIGHT) { Player.vector.y = ROOM_HEIGHT; }
 
 
-    //drawing grid;
-    CP_Graphics_ClearBackground(CP_Color_Create(128, 128, 128, 255));
+    //drawing lines;
+    CP_Graphics_ClearBackground(CP_Color_Create(0, 0, 0, 255));
+
+    CP_Settings_Stroke(CP_Color_Create(255, 255, 255, 255));
     for (int row = 0; row <= GRID_ROW; row++) {
         CP_Graphics_DrawLine(row * GRID_SIZE -camera1.x, 0 -camera1.y, row * GRID_SIZE -camera1.x, GRID_COL * GRID_SIZE - camera1.y);
     }
@@ -98,27 +135,39 @@ void game_update(void) {
     }
 
 
-
     //Player WASD example
     if (CP_Input_KeyDown(KEY_A)) {
-        Player.vector.x -= 20;
+        Player.vector.x -=Player.MOVE_SPEED;
     }
     if (CP_Input_KeyDown(KEY_D)) {
-        Player.vector.x += 20;
+        Player.vector.x += Player.MOVE_SPEED;
     }
     if (CP_Input_KeyDown(KEY_W)) {
-        Player.vector.y -= 20;
+        Player.vector.y -= Player.MOVE_SPEED;
     }
     if (CP_Input_KeyDown(KEY_S)) {
-        Player.vector.y += 20;
+        Player.vector.y += Player.MOVE_SPEED;
     }
 
     //Drawing Player Example
-    CP_Graphics_DrawCircle(Player.vector.x - camera1.x, Player.vector.y - camera1.y, 32);
+    CP_Image_Draw(PAWN, Player.vector.x- camera1.x, Player.vector.y - camera1.y, GRID_SIZE, GRID_SIZE, 255);
+
+
+
 
 
 }
 
 void game_exit(void) {
+
+    //freeing images
+    CP_Image_Free(&PAWN);
+    CP_Image_Free(&KNIGHT);
+    CP_Image_Free(&BISHOP);
+    CP_Image_Free(&ROOK);
+    CP_Image_Free(&QUEEN);
+    CP_Image_Free(&KING);
+
+
 
 }
